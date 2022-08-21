66d9680029ccdf994f81dbf959cc4ff5bf942d2d
Ralf Lang <ralf.lang@ralf-lang.de>
2022-08-21 06:30:30 +0000

    barebone checkin

---
+++ src/Registry.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Horde\Registry;
+
+/**
+ * The Registry
+ */
+class Registry
+{
+
+}
---
+++ test/RegistryTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright 2009-2021 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category Horde
+ * @package  Exception
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+
+namespace Horde\Registry\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Trivial tests for the Horde\Registry.
+ *
+ * @category Horde
+ * @package  Exception
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+class RegistryTest extends TestCase
+{
+}
